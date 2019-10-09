e546c4fae7ae758efdd5534c61309e2dc3f9e49c
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 02:17:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,18 +3,18 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
         #your key/value pairs here
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
@@ -22,18 +22,18 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age "50"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
