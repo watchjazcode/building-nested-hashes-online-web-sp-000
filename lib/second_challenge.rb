5191d928be189695c3bfbc4ab199b31626315cd5
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 02:03:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,9 +3,9 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => {},
-      :matriarch,
+      :matriarch => {},
       :hero,
-      :hero_friends
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
