93c78968ce61a300f1cd6fe72ec8ef7cff206615
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 02:13:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,10 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        
+        {:name
+        :age
+        :attitude},
+        {
       ]
    }, 
    :capulet => {
