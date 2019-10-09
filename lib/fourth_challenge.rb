2b9f3f3617dedba23d51d70bd944b62d8e32cc5b
Jazmin M <jazminmerckel@gmail.com>
2019-10-09 02:17:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,11 +6,11 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {:name => "Benvolio"
-         :age => "17"
+        {:name => "Benvolio",
+         :age => "17",
          :attitude => "worried"},
-        {:name => "Mercutio"
-         :age => "18"
+        {:name => "Mercutio",
+         :age => "18",
          :attitude => "hot-headed"}
       ]
    }, 
@@ -20,11 +20,11 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-       {:name => "Steven"
-        :age => "30"
+       {:name => "Steven",
+        :age => "30",
         :attitude => "confused"},
-       {:name => "Nurse"
-        :age => "44"
+       {:name => "Nurse",
+        :age => "44",
         :attitude => "worried"}
       ]
    }
